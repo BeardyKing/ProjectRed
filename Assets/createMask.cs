8c3aac2805a022407c56225cd0c95d09a3b38cf3
BeardyKing <khopking@gmail.com>
2018-12-12 17:22:18 +0000

    added
    meta data and misc bottom level content

---
+++ Assets/createMask.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createMask : MonoBehaviour {
+
+
+    SpriteMask mask;
+	// Use this for initialization
+	void Start () {
+        mask = gameObject.AddComponent<SpriteMask>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        mask.sprite = GetComponent<SpriteRenderer>().sprite;
+	}
+}
