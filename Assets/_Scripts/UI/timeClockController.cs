97ada945a223dc2918d213eaff5917bbc69baa14
BeardyKing <khopking@gmail.com>
2018-12-12 18:11:22 +0000

    Updated UI for in game
    + added UI and Prefab
    + level 1 now fades in

---
+++ Assets/_Scripts/UI/timeClockController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timeClockController : MonoBehaviour {
+
+	public GameObject arm;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float offset = (float)StaticData.BufferAge * 2f;
+
+		if (StaticData.CurrentAge == 1) {
+			arm.transform.eulerAngles = new Vector3(0, 0, 45 - offset);
+
+		} else if (StaticData.CurrentAge == 2) {
+			arm.transform.eulerAngles = new Vector3(0, 0, 0 - offset);
+
+		} else if(StaticData.CurrentAge == 3){
+			arm.transform.eulerAngles = new Vector3(0, 0, -45 - offset);
+		}
+
+
+	}	
+}
