1819f9a507ed8418f8e8ad2fa21820b10128df32
BeardyKing <khopking@gmail.com>
2018-12-12 17:20:14 +0000

    added
    from kingsley with love
    levels
    materials
    post-processing

---
+++ Assets/From Kingsley with love/LoadLevel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour {
+
+	int levelSelected;
+	bool singlePass = false;
+	public MakeTransition box;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SelectLevelNow(int input){
+		SceneManager.LoadScene(input);
+
+	}
+	public void SelectLevelWait(int input){
+		levelSelected = input;
+		if (singlePass == false) {
+			singlePass = true;
+			box.startTransition = true;
+		}
+		Invoke("WaitBeforeLoad", 1);
+	}
+
+	void WaitBeforeLoad(){
+		SceneManager.LoadScene(levelSelected);
+	}
+}
---
+++ Assets/From Kingsley with love/MakeTransition.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MakeTransition : MonoBehaviour {
+
+
+	Image box;
+	public bool startTransition = false;
+	public bool startedLevel = false;
+
+	// Use this for initialization
+	void Start () {
+		startedLevel = true;
+		box = GetComponent<Image>();
+		box.color = new Color(box.color.r, box.color.g, box.color.b, 1f);
+		//Debug.Log(box.color.a);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (startedLevel == true) {
+			box.color = new Color(box.color.r, box.color.g, box.color.b, box.color.a - (2f * Time.deltaTime));
+			if (box.color.a <= 0) {
+				startedLevel = false;
+			}
+		}
+		if (startTransition == true) {
+			box.color = new Color(box.color.r, box.color.g, box.color.b, box.color.a + (1f*Time.deltaTime));
+			//if (box.color.a >= 2) {
+			//	startTransition = false;
+			//}
+		}
+	}
+
+
+}
