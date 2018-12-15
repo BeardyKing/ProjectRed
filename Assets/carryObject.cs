1f7b4bd3f713b0ab37d00f283bab72a87fa246e6
Oliver <ojaypopedev@gmail.com>
2018-12-15 09:07:02 +0000

    Cleaned up tilemaps
    Cleanup of tile maps
    Level 3 edited and improved

---
+++ Assets/carryObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carryObject : MonoBehaviour {
+
+
+    Transform parent;
+	// Use this for initialization
+	void Start () {
+        parent = transform.parent;
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" ||collision.gameObject.tag == "Ground")
+        collision.transform.parent = parent;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Ground")
+            collision.transform.parent = null;
+    }
+
+   
+}
