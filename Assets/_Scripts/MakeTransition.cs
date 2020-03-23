c38d33d7e197acf1680741a8deea72ab0854bc66
BeardyKing <39307332+BeardyKing@users.noreply.github.com>
2020-03-23 19:36:37 +0000

    Project folder clean up
    Cleaned up the project folder and added missing level file

---
+++ Assets/_Scripts/MakeTransition.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MakeTransition : MonoBehaviour {
+
+
+	Image box;
+	public bool startTransition = false;
+	public bool startedLevel = false;
+	float timer;
+	float maxTime = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+		startedLevel = true;
+		box = GetComponent<Image>();
+		box.color = new Color(box.color.r, box.color.g, box.color.b, 1f);
+		//Debug.Log(box.color.a);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.name == "blackBox_lvl1") {
+			timer += Time.deltaTime;
+			if (startedLevel == true && timer > maxTime) {
+				box.color = new Color(box.color.r, box.color.g, box.color.b, box.color.a - (0.5f * Time.deltaTime));
+				if (box.color.a <= 0) {
+					startedLevel = false;
+				}
+			}
+			if (startTransition == true) {
+				box.color = new Color(box.color.r, box.color.g, box.color.b, box.color.a + (1f * Time.deltaTime));
+				//if (box.color.a >= 2) {
+				//	startTransition = false;
+				//}
+			}
+		}
+
+
+        else if (gameObject.name == "blackBox_end")
+        {
+            timer += Time.deltaTime;
+            if (startedLevel == true && timer > maxTime)
+            {
+                box.color = new Color(box.color.r, box.color.g, box.color.b, box.color.a - (0.5f* Time.deltaTime));
+                if (box.color.a <= 0)
+                {
+                    startedLevel = false;
+                }
+            }
+            if (startTransition == true)
+            {
+                box.color = new Color(box.color.r, box.color.g, box.color.b, box.color.a + (0.25f * Time.deltaTime));
+                //if (box.color.a >= 2) {
+                //  startTransition = false;
+                //}
+            }
+        }
+        else
+        {
+            if (startedLevel == true)
+            {
+                box.color = new Color(box.color.r, box.color.g, box.color.b, box.color.a - (2f * Time.deltaTime));
+                if (box.color.a <= 0)
+                {
+                    startedLevel = false;
+                }
+            }
+            if (startTransition == true)
+            {
+                box.color = new Color(box.color.r, box.color.g, box.color.b, box.color.a + (1f * Time.deltaTime));
+                //if (box.color.a >= 2) {
+                //  startTransition = false;
+                //}
+            }
+        }
+
+    }
+
+
+}
---
+++ Assets/_Scripts/UI_Animation_Aim.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Animation_Aim : MonoBehaviour
+{
+	public FrozenObjects frozenObjects;
+	public GameObject aim_long;
+	public GameObject aim_ball;
+	public GameObject mouse_click;
+	public GameObject mouse_click_right;
+
+	public UITriggerController uiTriggerRef;
+
+	public Color white;
+	//public Color red;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	// Update is called once per frame
+	float counter;
+	float maxCounter = 0.3f;
+	bool colActive = false;
+	int temp = 0;
+    void Update()
+    {
+		if (uiTriggerRef.uiState == 2) {
+			counter += Time.deltaTime;
+			if (counter > maxCounter) {
+				counter = 0;
+				colActive = !colActive;
+				if (colActive == true) {
+					aim_long.GetComponent<SpriteRenderer>().color = white;
+					aim_ball.GetComponent<SpriteRenderer>().color = white;
+					mouse_click.GetComponent<SpriteRenderer>().color = white;
+					mouse_click_right.GetComponent<SpriteRenderer>().color = white;
+				} 
+				else if(colActive == false) {
+
+					aim_long.GetComponent<SpriteRenderer>().color = frozenObjects.colors[temp];
+					aim_ball.GetComponent<SpriteRenderer>().color = frozenObjects.colors[temp];
+					if (temp == 0) {
+						mouse_click.GetComponent<SpriteRenderer>().color = frozenObjects.colors[0];
+					}
+					if (temp == 1) {
+						mouse_click_right.GetComponent<SpriteRenderer>().color = frozenObjects.colors[1];
+					}
+
+					temp++;
+					if (temp > 1) {
+						temp = 0;
+					}
+				}
+			}
+		}
+
+	}
+}
